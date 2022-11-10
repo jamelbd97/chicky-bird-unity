e6d1721f052c29b417f00b5fadb2b3ebf191ee2c
Maherrrrr <maher.ali@esprit.tn>
2022-11-10 11:33:07 +0100

    Android build / Accelerometre

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -5,51 +5,57 @@ using UnityEngine.UI;
 
 public class GameBehavior : MonoBehaviour
 {
-    // 1 = Gyro / 2 = Accelero / 3 = Camera  
-    // Variable accessible men ay script
-    public static int gameMode;
-    public static bool gameRunning = false;
+	// 0 = Keyboard / 1 = Gyro / 2 = Accelero / 3 = Camera  
+	// Variable accessible men ay script
+	public static int gameMode;
+	public static bool gameRunning = false;
 
 	public Text topText;
-	public GameObject gyroButton;
-	public GameObject aceleroButton;
-	public GameObject cameraButton;
+	public GameObject menu;
 
-    void Start()
-    {
-        gameMode = 0;
-    }
+	void Start()
+	{
+		gameMode = 0;
+	}
 
-    void Update()
-    {
-        
-    }
+	void Update()
+	{
 
-    public void chooseMode(int modeCode){
-        gameMode = modeCode;
+	}
 
-        if (modeCode == 1){
-            topText.text = "Gyroscope";
-        } else if (modeCode == 2){
-            topText.text = "Accelerometre";
-        } else if (modeCode == 3) {
-            topText.text = "Camera";
-        }
+	public void chooseMode(int modeCode)
+	{
+		gameMode = modeCode;
 
-        gyroButton.SetActive(false);
-        aceleroButton.SetActive(false);
-        cameraButton.SetActive(false);
+		if (modeCode == 1)
+		{
+			topText.text = "Gyroscope";
+		}
+		else if (modeCode == 2)
+		{
+			topText.text = "Accelerometre";
+		}
+		else if (modeCode == 3)
+		{
+			topText.text = "Camera";
+		}
+		else
+		{
+			topText.text = "Keyboard";
+		}
 
-        startGame();
+		menu.SetActive(false);
+
+		startGame();
 	}
 
 	public static void startGame()
 	{
-        gameRunning = true;
+		gameRunning = true;
 	}
 
 	public static void stopGame()
 	{
-        gameRunning = false;
+		gameRunning = false;
 	}
 }
--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,35 +3,28 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour
 {
 
-    public Animator animator;
-    public Rigidbody rigidBody;
+	public Animator animator;
+	public Rigidbody rigidBody;
 	public float jumpforce = 10f;
 
 	// Start is called before the first frame update
 	void Start()
-    {
-		
+	{
+
 	}
 
 	// Update is called once per frame
 	void Update()
-    {
-		if (Input.GetKeyDown("up"))
+	{
+		if (GameBehavior.gameMode == 2)
 		{
-			rigidBody.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
-			animator.SetBool("Flying", true);
+			useAccelerometer();
 		}
-
-		if (Input.GetKeyDown("right"))
-        {
-            transform.position += Vector3.right;
-        }
-
-        if (Input.GetKeyDown("left"))
-        {
-            transform.position += Vector3.left;
-        }
-    }
+		else
+		{
+			useKeyboard();
+		}
+	}
 
 	private void OnTriggerEnter(Collider collider)
 	{
@@ -40,4 +33,35 @@ public class PlayerBehavior : MonoBehaviour
 			GameBehavior.stopGame();
 		}
 	}
+
+	private void useKeyboard()
+	{
+		if (Input.GetKeyDown("up"))
+		{
+			rigidBody.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+			animator.SetBool("Flying", true);
+		}
+
+		if (Input.GetKeyDown("right"))
+		{
+			transform.position += Vector3.right;
+		}
+
+		if (Input.GetKeyDown("left"))
+		{
+			transform.position += Vector3.left;
+		}
+	}
+
+	private void useAccelerometer()
+	{
+		if (Input.GetKeyDown("up"))
+		{
+			rigidBody.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+			animator.SetBool("Flying", true);
+		}
+
+		transform.Translate(Input.acceleration.x, Input.acceleration.y, 0);
+	}
+
 }
